38aab87526cea3be424cc6188270395d3b5fdcb6
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-08 02:04:34 +0300

    Реализован скрипт для pretty print'a json файлов
    Добавлена функция load_data, принимающая на вход путь до файла в формате json и позволяющая распарсить его.
    
    Добавлена функция json_pretty_print, принимающая на вход обработанный json файл и печатающая его в удобочитаемом виде.
    
    Добавлен ввод пользователем пути до файла в формате json и проверка на существование файла.

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
-import json
+# -*- coding: utf-8 -*-
+from json import load, dumps
+from os.path import exists
 
 
 def load_data(filepath):
-    pass
+    if not exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return load(file_handler)
 
 
-def pretty_print_json(data):
-    pass
+def json_pretty_print(data):
+    print dumps(data, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = raw_input(u'Введите путь к файлу в формате json:')
+    data = load_data(filepath)
+    if data is None:
+        print (u'Данных нет или указан неверный путь к файлу')
+    else:
+        json_pretty_print(data)
+
