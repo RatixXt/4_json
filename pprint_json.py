de521b0b9a6c4f0b0df2e2bfde8d4bee2984f335
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-09 23:28:53 +0300

    Внесены поправки в связи с переходом на Python 3.5
    Использован open из библиотеки codecs. 
    
    Поставлены скобочки у функции print().

--- pprint_json.py
+++ pprint_json.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
-from json import load, dumps
+from json import dumps, load
+from codecs import open
 from os.path import exists
 
 
 def load_data(filepath):
     if not exists(filepath):
         return None
-    with open(filepath, 'r') as file_handler:
+
+    with open(filepath, 'r', 'utf-8') as file_handler:
         return load(file_handler)
 
 
 def json_pretty_print(data):
-    print dumps(data, ensure_ascii=False, indent=4)
+    print(dumps(data, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    filepath = raw_input(u'Введите путь к файлу в формате json:')
+    filepath = input(u'Введите путь к файлу в формате json:')
     data = load_data(filepath)
     if data is None:
         print (u'Данных нет или указан неверный путь к файлу')
     else:
         json_pretty_print(data)
-
